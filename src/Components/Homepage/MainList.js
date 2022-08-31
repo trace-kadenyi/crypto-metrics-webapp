2cc2a805c226c46d10f4229be9d65afd0ccab08b
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-08-31 15:29:14 +0300

    Add functionality to the search bar

--- src/Components/Homepage/MainList.js
+++ src/Components/Homepage/MainList.js
@@ -12,19 +12,15 @@ const MainList = () => {
     dispatch(fetchCryptoData());
   }, [dispatch]);
 
+  const filtered = crypto.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <div>
       <div className="search_bar">
         <input className="search" type="text" placeholder="Search" onChange={(e) => setSearch(e.target.value)} />
       </div>
       <ul className="main_list">
-        {crypto.filter((item) => {
-          if (search === '') {
-            return item;
-          } if (item.name.toLowerCase().includes(search.toLowerCase()) || item.symbol.toLowerCase().includes(search.toLowerCase()) || item.rank.includes(search)) {
-            return item;
-          }
-        }).map((item) => (
+        {filtered.map((item) => (
           <Home
             key={item.id}
             id={item.id}
